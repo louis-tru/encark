686ae6fa613182f751337698e7baa4b1e3c331fb
louistru <louistru@hotmail.com>
2020-06-02 18:29:11 +0800

    up: add sync_watch delay options

--- sync_watch.ts
+++ sync_watch.ts
@@ -8,8 +8,9 @@ import { execSync } from './syscall';
 defOpts('help', 0,									'--help, --help print help info');
 defOpts('u', 'louis',								'-u username [{0}]');
 defOpts('h', '192.168.0.115',				'-h host [{0}]');
-defOpts('t', '~/ngui',              '-t target directory [{0}]');
+defOpts('t', '~/ngui',							'-t target directory [{0}]');
 defOpts('i', '',										'-i ignore directory or file');
+defOpts('d',  0,										'-d delay time [{0}] watch');
 
 if (options.help) {
 	console.log(' ', helpInfo.join('\n  '));
@@ -73,31 +74,37 @@ function sync(type: string, dir: string, filename: string) {
 	console.log('sync', type, dir, filename, r.code == 0 ? 'ok': 'fail');
 }
 
-fs.watch(root, (type, filename)=>sync(type, '.', filename));
+function start() {
 
-each_directory(root, '', function(pathname: string, name: string, ext: string, is_dir: boolean) {
-	if (is_dir) {
-		if (ignore.indexOf(name) >= 0 || 
-				ignore.indexOf(pathname) >= 0 ||
-				ignore.indexOf(ext) >= 0
-		) {
-			// console.log(pathname);
-			return false;
+	fs.watch(root, (type, filename)=>sync(type, '.', filename));
+
+	each_directory(root, '', function(pathname: string, name: string, ext: string, is_dir: boolean) {
+		if (is_dir) {
+			if (ignore.indexOf(name) >= 0 || 
+					ignore.indexOf(pathname) >= 0 ||
+					ignore.indexOf(ext) >= 0
+			) {
+				// console.log(pathname);
+				return false;
+			}
+			// console.log('-------------', root + '/' + pathname)
+			fs.watch(root + '/' + pathname, (type, filename)=>sync(type, pathname, filename));
+			count++;
+			return true;
 		}
-		// console.log('-------------', root + '/' + pathname)
-		fs.watch(root + '/' + pathname, (type, filename)=>sync(type, pathname, filename));
-		count++;
-		return true;
-	}
-	return false;
-});
+		return false;
+	});
 
-// sudo ulimit -HSn 12000
+	// sudo ulimit -HSn 12000
 
-execSync(`cd ${root}; git status -s | awk '{print $2}'`).stdout.forEach(e=>{
-	if (e) {
-		sync('init', path.dirname(e), path.basename(e));
-	}
-});
+	execSync(`cd ${root}; git status -s | awk '{print $2}'`).stdout.forEach(e=>{
+		if (e) {
+			sync('init', path.dirname(e), path.basename(e));
+		}
+	});
 
-console.log(`---------------- Start watch dir count ${count} ... ---------------- `);
+	console.log(`---------------- Start watch dir count ${count} ... ---------------- `);
+
+}
+
+setTimeout(start, Number(options.d) || 0);
