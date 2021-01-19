210848be0f3e462fb9380acf559933f867874b41
louistru <louistru@hotmail.com>
2021-01-19 19:53:41 +0800

    update

--- _.js
+++ _.js
@@ -6,13 +6,6 @@ var pkg = JSON.parse(
 	fs.readFileSync(`${__dirname}/package.json`, 'utf8')
 );
 
-pkg.types = pkg.main.replace(/\.js/, '.d.ts');
-
-fs.writeFileSync(
-	`${__dirname}/out/${pkg.name}/package.json`,
-	JSON.stringify(pkg, null, 2)
-);
-
 const _buffer = `${__dirname}/out/${pkg.name}/_buffer.js`;
 
 fs.writeFileSync(
