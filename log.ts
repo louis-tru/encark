cf8713b470c8a8904262a0a63ff8c053e4370de5
chuxuewen <louistru@hotmail.com>
2022-01-26 14:17:54 +0800

    update log

--- log.ts
+++ log.ts
@@ -188,7 +188,7 @@ export class Console extends Notification {
 	}
 
 	warn(msg: any, ...args: any[]) {
-		return this._print('WARN', 'color: #777700; background: #eeff00', log, msg, ...args);
+		return this._print('WARN', 'color: #a89d7f; background: #fffbe9', log, msg, ...args);
 	}
 
 	error(msg: any, ...args: any[]) {
