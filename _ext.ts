dca00a2cc31330a107c2e3f3c2755063430fe87f
李学研 <lixueyan@stars-mine.com>
2022-03-31 12:43:25 +0800

    修改parent类型

--- _ext.ts
+++ _ext.ts
@@ -45,7 +45,7 @@ interface NodeModule {
 	id: string;
 	filename: string;
 	loaded: boolean;
-	parent: NodeModule | null; // | undefined;
+	parent: NodeModule | null | undefined;
 	children: NodeModule[];
 	paths: string[];
 	package?: any;
